f6765cbb957d4d25b68091595a53573b4555cb46
back7 <back7@dev.lan>
2021-10-26 14:21:04 +0300

    Created test and main env.

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent
+
+if os.environ.get('ENV') == 'test':
+    filepath = os.path.join(BASE_DIR, ".env.test")
+else:
+    filepath = os.path.join(BASE_DIR, ".env")
+
+load_dotenv(filepath)
 
 API_KEY = os.environ.get('API_KEY')
 API_SECRET = os.environ.get('API_SECRET')
