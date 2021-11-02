8de9919b2822d9faa13d99c7c332d11f8714cb76
back7 <back7@dev.lan>
2021-11-02 22:24:09 +0300

    Added plots and analytics.

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-import numpy as np
-from flask import Flask, request
-import pandas as pd
+from flask import Flask, render_template
 
-from utils import get_active_list
+from services import get_active_list_dataset, get_active_list_hist, get_active_list_info, get_maturity_date_hist
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    active_list = get_active_list()
-    df = pd.DataFrame(active_list)
-    df = df.astype({'size': 'int64', 'dailyIntRate': 'float'})
-    df_grouped: pd.DataFrame = df.groupby(pd.cut(df["dailyIntRate"], np.arange(0, 0.0015, 0.0001))).sum()[["size"]]
-    return df_grouped.to_html()
+    df = get_active_list_dataset()
+    active_list_distr_image = get_active_list_hist(df)
+    maturity_date_image = get_maturity_date_hist(df)
+    kwargs = get_active_list_info(df)
+    return render_template(
+        "plot.html", active_list_distr_image=active_list_distr_image, maturity_date_image=maturity_date_image, **kwargs
+    )
