7c9e51d4bbfffff592b162e51a09cf8b38a2b02a
back7 <back7@dev.lan>
2021-12-07 22:17:04 +0300

    Remove rent script.

--- celery_app.py
+++ celery_app.py
@@ -7,12 +7,4 @@ celery_app.conf.beat_schedule = {
         "task": "lend.lend",
         "schedule": 20.0,
     },
-    "process-new": {
-        "task": "rent.new_ads",
-        "schedule": 300.0,
-    },
-    "process-upped": {
-        "task": "rent.upped_ads",
-        "schedule": 270.0,
-    },
 }
--- run_tasks.py
+++ run_tasks.py
@@ -1,5 +1,4 @@
 # Module to run celery scheduled tasks
 
 from lend import lend
-from rent import new_ads, upped_ads
 from celery_app import celery_app
